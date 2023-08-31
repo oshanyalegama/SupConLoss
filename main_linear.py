a58848fc96a18c253afc37f84f8a6da94313b6f4
oshanyalegama <110217717+oshanyalegama@users.noreply.github.com>
2023-08-31 16:15:16 +0530

    Update main_linear.py

--- main_linear.py
+++ main_linear.py
@@ -1,15 +1,6 @@
 from __future__ import print_function
 from config.config_linear import parse_option
-from training_linear.training_one_epoch_ckpt import main
-from training_linear.training_one_epoch_fusion import main_supervised_fusion
-from training_linear.training_one_epoch_supervised import main_supervised
-from training_linear.training_one_epoch_supervised_multilabel import main_supervised_multilabel
-from training_linear.training_one_epoch_fusion_multilabel import main_supervised_multilabel_fusion
-from training_linear.training_one_epoch_ckpt_multi import main_multilabel
-from training_linear.training_one_epoch_ckpt_bce import main_bce
-from training_linear.training_one_epoch_transformer import main_transformer
-from training_linear.training_one_epoch_transformer_multilabel import main_transformer_multilabel
-from training_linear.training_one_epoch_ckpt_student_teacher import main_student_teacher
+from training_one_epoch_ckpt_multi import main_multilabel
 try:
     import apex
     from apex import amp, optimizers
